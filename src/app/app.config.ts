804673ebf930a779bddb62f5f197fd94c08b7506
AtiposEiji <davide.brignoli12@gmail.com>
2024-06-21 21:52:05 +0200

    test redirect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
