8273eb2cfa5341003bf9eccf7b0689dbe9346711
AtiposEiji <davide.brignoli12@gmail.com>
2024-06-21 21:59:40 +0200

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    {
-      provide: LocationStrategy,
-      useClass: HashLocationStrategy
-    }
+    provideRouter(routes)
   ]
 };
