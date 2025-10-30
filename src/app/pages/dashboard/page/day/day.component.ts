5e0d1363252ec9c3ea1eca9dfcb05306c01f434e
Davide Brignoli <68071016+AtiposEiji@users.noreply.github.com>
2025-10-30 11:54:11 +0100

    Update day.component.ts

--- src/app/pages/dashboard/page/day/day.component.ts
+++ src/app/pages/dashboard/page/day/day.component.ts
@@ -27,56 +27,7 @@ export class DayComponent {
     this.dayId = Number(this.activatedRoute.snapshot.paramMap.get('dayId'));
   }
 
-  protected cookedQuantity(data: FoodDataModel): string | number {
-    switch (data.idFood) {
-      case FOOD.PASTA_RISO_ASCIUTTI:
-      case FOOD.PASTA_RISO_BRODO:
-      case FOOD.FARRO_ORZO:
-      case FOOD.TORTELLINI_ASCIUTTI:
-      case FOOD.TORTELLINI_BRODO:
-      case FOOD.GNOCCHI_PATATE:
-      case FOOD.LASAGNE:
-      case FOOD.POLLO_CONIGLIO:
-      case FOOD.TACCHINO_FARAONA:
-      case FOOD.AGNELLO_MAIALE:
-      case FOOD.TRIPPA:
-      case FOOD.PESCE_GENERALE:
-      case FOOD.MITILI_SEPPIA_RAZZA:
-      case FOOD.CALAMARO_TROTA:
-      case FOOD.BACCALA_STOCCAFIS:
-      case FOOD.WURSTEL:
-      case FOOD.PATATE:
-      case FOOD.FAGIOLI:
-      case FOOD.PISELLI_FAGIOLINI:
-      case FOOD.CAROTE:
-      case FOOD.CECI:
-      case FOOD.CAVOLFIORE:
-      case FOOD.ZUCCHINE:
-      case FOOD.PURE:
-      case FOOD.FINOCCHI:
-      case FOOD.VERDURE:
-        return this.calculateCookedQuantity(data);
-      default:
-        return data.quantity;
-    }
-  }
-
   protected goBack() {
     return this.router.navigate([`season/${this.seasonId}`]);
-  }
-
-  private calculateCookedQuantity(data: FoodDataModel): string {
-    let result: number = Number(data.quantity);
-    const allDishes = [
-      ...foodListData.firstDishes,
-      ...foodListData.secondDishes,
-      ...foodListData.sideDishes
-    ];
-    allDishes.forEach(dish => {
-      if (data.idFood === dish.idFood) {
-        result = (dish.cookedWeight * Number(data.quantity)) / dish.rawWeight;
-      }
-    });
-    return `${Math.round(result)}g cotto`;
   }
 }
